47047640a6668d54ee370441e04ad27735a3f6c6
pikktorr <viktor.rozgonyi@gmail.com>
2020-10-01 21:20:01 +0200

    structure

---
+++ src/Containers/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+import Banner from "./Banner";
+import Body from "./Body";
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
